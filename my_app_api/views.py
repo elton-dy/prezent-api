c768e659e5335b076afa580bda8713edb3985442
edoisy <edoisy@relatia.fr>
2023-09-30 19:39:40 +0200

    feat:llm add memory

---
+++ my_app_api/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
+from .ai_handler import conversational_chat
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == 201:
+            conversation = self.serializer_class().Meta.model.objects.get(pk=response.data['id'])
+            Message.objects.create(
+                conversation=conversation,
+                text="Bonjour ! Comment puis-je vous aider aujourd'hui ?",
+                type="AI"
+            )
+        return response
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_message = request.data.get('text', '')  # Obtenez le texte du message de l'utilisateur
+        conversation_id = request.data.get('conversation', None)  # Obtenez l'ID de la conversation
+
+        if not conversation_id:
+            return Response({'error': 'Conversation ID is required'}, status=400)
+
+        try:
+            conversation = Conversation.objects.get(pk=conversation_id)  # Récupérez l'objet Conversation
+        except Conversation.DoesNotExist:
+            return Response({'error': 'Invalid Conversation ID'}, status=400)
+
+        # Créez le message utilisateur dans la base de données
+        response = super().create(request, *args, **kwargs)
+
+        if response.status_code == 201:  # Si le message de l'utilisateur a été créé avec succès
+            ai_response = conversational_chat(user_message, conversation)  # Obtenez la réponse de l'IA
+
+            # Créez le message de l'IA dans la base de données
+            Message.objects.create(
+                conversation=conversation,
+                text=ai_response,
+                type="AI"
+            )
+
+        return response  # Retournez la réponse HTTP originale
--- pzt_api/urls.py
+++ pzt_api/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from myapp.views import UserViewSet, ConversationViewSet, MessageViewSet
+from my_app_api.views import UserViewSet, ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
