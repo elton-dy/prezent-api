3d4b4703c5f8d06c66bfa1b1b351bab0083d8355
edoisy <edoisy@relatia.fr>
2023-09-30 21:16:31 +0200

    feat:llm conversation ok

--- my_app_api/ai_handler.py
+++ my_app_api/ai_handler.py
@@ -1,12 +1,11 @@
 import os
 from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from my_app_api.models import Message
-from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+
 
 os.environ["OPENAI_API_KEY"] = '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'
 
@@ -16,72 +15,63 @@ class DatabaseMemory:
 
     def get_history(self):
         messages = Message.objects.filter(conversation=self.conversation).order_by('timestamp')
-        history = [(msg.text, msg.type) for msg in messages]
-        formatted_history = '\n'.join(f'{item[0]}: {item[1]}' for item in history)
+        history = [(f"{msg.type}: {msg.text}") for msg in messages]
+        formatted_history = '\n'.join(history)
         return formatted_history
+
+custom_prompt_template = """
+Répondez en français.
+comporte toi comme un conseille qui aide a trouver le cadeau parfait, pose des question a l'utilisateur pour savoir quel cadeau proposer.
+Utilisez d'abord l'historique de la conversation, puis le contexte.
+Utilisez le contexte suivant (délimité par <ctx></ctx>) et l'historique de la conversation (délimité par <hs></hs>) pour répondre à la question :
+------
+<ctx>
+{context}
+</ctx>
+------
+
+------
+{question}
+Réponse :
+"""
+
+def set_custom_prompt():
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
 
 def get_vectorstore():
     return Chroma(persist_directory="data/db/news", embedding_function=OpenAIEmbeddings())
 
-def get_custom_prompt():
-    template = """
-    Répondez en français.
-    comporte toi comme un conseille qui aide a trouver le cadeau parfait, pose des question a l'utilisateur pour savoir quel cadeau proposer.
-    Utilisez d'abord l'historique de la conversation, puis le contexte.
-    Utilisez le contexte suivant (délimité par <ctx></ctx>) et l'historique de la conversation (délimité par <hs></hs>) pour répondre à la question :
-    ------
-    <ctx>
-    {context}
-    </ctx>
-    ------
-    <hs>
-    {history}
-    </hs>
-    ------
-    {question}
-    Réponse :"""
+def load_llm():
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    return llm
 
-    return PromptTemplate(
-        input_variables=["history", "context", "question"],
-        template=template,
-    )
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                           chain_type='stuff',
+                                           retriever=db.as_retriever(search_kwargs={'k': 1}),
+                                           return_source_documents=False,
+                                           chain_type_kwargs={'prompt': prompt}
+                                           )
+    return qa_chain
 
-def get_llm():
-    return ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+def qa_bot(conversation):
+    db = get_vectorstore()
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+    return qa
 
-def get_memory(conversation):
-    db_memory = DatabaseMemory(conversation)
-    return ConversationBufferMemory(
-            human_prefix='User',
-            chat_memory=db_memory.get_history)
-def get_chain(conversation):
-    db_memory = DatabaseMemory(conversation)
-    # Convertir l'objet DatabaseMemory en un dictionnaire
-    memory_dict = {
-        "history": db_memory.get_history(),
-    }
-
-    return RetrievalQA.from_chain_type(
-        llm=get_llm(),
-        chain_type="stuff",
-        retriever=get_vectorstore().as_retriever(search_kwargs={"k":1}),
-        return_source_documents=False,
-        verbose=True,
-        chain_type_kwargs={
-            "verbose": True,
-            "prompt": get_custom_prompt(),
-            "memory": get_memory(conversation)
-        }
-    )
+def final_result(query, conversation, db_memory):
+    qa_result = qa_bot(conversation)
+    response = qa_result.run({'query': query, 'context': ''})
+    return response
 
 def conversational_chat(query, conversation):
-    memory = DatabaseMemory(conversation)
-    formatted_history = memory.get_history()
-    print('ici')
-    response = get_chain(conversation).run({
-        "query": query,
-        'history': formatted_history
-    })
+    db_memory = DatabaseMemory(conversation)
+    response = final_result(query, conversation, db_memory)
+    print('ICI:')
     print(response)
-    exit()
-    return response  # Supposons que response contient le texte de réponse de l'IA
+    print('ICI:')
+    return response  # Suppose que response contient le texte de réponse de l'IA
