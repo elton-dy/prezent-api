573314cfb35519317bb5917743be331b8e4a227d
edoisy <edoisy@relatia.fr>
2023-12-21 16:53:41 +0100

    feat:setting maj secure

---
+++ my_app_api/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import BasePermission
+from .models import Visitor
+
+class IsAuthenticatedOrVisitorWithUUID(BasePermission):
+    def has_permission(self, request, view):
+        # Autoriser les utilisateurs authentifiés
+        if request.user.is_authenticated:
+            return True
+
+        # Autoriser les visiteurs avec un UUID valide
+        visitor_uuid = request.GET.get('visitor_uuid') or request.data.get('visitor_uuid')
+        if visitor_uuid:
+            return Visitor.objects.filter(uuid=visitor_uuid).exists()
+
+        return False
+
+class CreateOrReadOnly(BasePermission):
+    """
+    Permission personnalisée pour permettre l'accès non authentifié aux requêtes POST et GET,
+    mais exiger l'authentification pour les autres méthodes.
+    """
+
+    def has_permission(self, request, view):
+        # Autoriser POST et GET sans authentification
+        if request.method in ['POST', 'GET']:
+            return True
+
+        # Pour les autres requêtes, exiger l'authentification
+        return request.user and request.user.is_authenticated
